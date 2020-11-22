773665ec986172c5193cb482c2d137422fad476a
xiongmao86 <xiongmao86dev@sina.com>
2020-11-22 12:51:03 +0800

    Success in loading binary file.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,6 +1,10 @@
 describe("Added libary jasmine", function() {
+    const fs = require('fs');
+    const path = require('path');
+    const binData = fs.readFileSync(
+        path.join(__dirname, "assets", "dns_response.binary"));
+
     it("should pass", function() {
-        let testPassed =true;
-        expect(testPassed).toBe(true);
+        expect(binData.length).toBe(260);
     });
 });
