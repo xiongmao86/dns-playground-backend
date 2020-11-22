9856d0e9eb8cdcfcf9630453db5fe45f4e2cc665
xiongmao86 <xiongmao86dev@sina.com>
2020-11-22 16:54:16 +0800

    Test setup.

--- spec/Parser.spec.mjs
+++ spec/Parser.spec.mjs
@@ -2,13 +2,9 @@ import fs from 'fs';
 import Parser from '../Parser.js'
 
 const binData = fs.readFileSync('./spec/assets/dns_response.binary');
+let parser = new Parser(binData);
 
 describe("Parser", () => {
-    let parser;
-    beforeEach(() => {
-        parser = new Parser(binData);
-    })
-
     it("Setup successful.", () => {
         expect(parser.bufferLength()).toBe(260);
     })
--- spec/index.spec.mjs
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-
-const binData = fs.readFileSync('./spec/assets/dns_response.binary');
-
-describe("Added libary jasmine", function() {
-    it("should pass", function() {
-        expect(binData.length).toBe(260);
-    });
-});
