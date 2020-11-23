ee440d9df94ed925ee6d2be306c19374cd23f3e2
xiongmao86 <xiongmao86dev@sina.com>
2020-11-23 09:43:50 +0800

    Parsing id and flags.

--- Parser.js
+++ Parser.js
@@ -2,20 +2,54 @@ class Parser {
     constructor(buffer) {
         this.buffer = buffer;
         this.i = 0;
-        // return this;
     }
-    readUInt16() {
-        let result = this.buffer.readUInt16LE(i);
-        this.i += 2;
-        return 
-    }
+
     bufferLength() {
         let buffer = this.buffer;
         return buffer? buffer.length : 0;
     }
-    parse() {
+
+    readUInt16() {
+        let result = this.buffer.readUInt16BE(this.i);
+        this.i += 2;
+        return result;
+    }
+
+    parse_flags(flagbits) {
+        let qr = flagbits >>> 15;
+        let opcode = flagbits >>> 11 & 0b1111;
+        let aa = flagbits >>> 10 & 1;
+        let tc = flagbits >>> 9 & 1;
+        let rd = flagbits >>> 8 & 1;
+        let ra = flagbits >>> 7 & 1;
+        let z = flagbits >>> 6 & 1;
+        let ad = flagbits >>> 5 & 1;
+        let cd = flagbits >>> 4 & 1;
+        let rcode = flagbits & 0b1111;
+
         return {
-            id: this.readUInt16()
+            response: qr,
+            opcode,
+            authoritative: aa,
+            truncated: tc,
+            recursion_desired: rd,
+            recursion_available: ra,
+            z,
+            authentic_data: ad,
+            checking_disable: cd,
+            error_code: rcode
+        }
+    }
+
+    parse() {
+        let id = "0x" + this.readUInt16().toString(16).padStart(4, '0');
+    
+        let flagbits = this.readUInt16();
+        let flags = this.parse_flags(flagbits);
+
+        return {
+            id,
+            flags
         }
     }
 }
--- spec/Parser.spec.mjs
+++ spec/Parser.spec.mjs
@@ -1,11 +1,37 @@
 import fs from 'fs';
 import Parser from '../Parser.js'
 
-const binData = fs.readFileSync('./spec/assets/dns_response.binary');
-let parser = new Parser(binData);
-
 describe("Parser", () => {
+    let parser;
+    let result;
+    beforeEach(() => {
+        const binData = fs.readFileSync('./spec/assets/dns_response.binary');
+        parser = new Parser(binData);
+        result = parser.parse();
+    })
     it("Setup successful.", () => {
         expect(parser.bufferLength()).toBe(260);
     })
+
+    it("should parse id", () => {
+        expect(result.id).toBe("0x867f");
+    })
+
+    it("should parse flags", () => {
+        let flags = result.flags;
+        expect(flags.response).toBe(1);
+        expect(flags.opcode).toBe(0);
+        expect(flags.authoritative).toBe(0);
+        expect(flags.truncated).toBe(0);
+        expect(flags.recursion_desired).toBe(1);
+        expect(flags.recursion_available).toBe(1);
+        expect(flags.z).toBe(0);
+        expect(flags.authentic_data).toBe(0);
+        expect(flags.checking_disable).toBe(0);
+        expect(flags.error_code).toBe(0);
+    })
+
+    // it("should parse counts", () => {
+    //     expect(result.query_count).toBe(1);
+    // })
 })
