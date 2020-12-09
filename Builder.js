26b44b16638fa08c5fcf94e951bfaeee9736ea37
xiongmao86 <xiongmao86dev@sina.com>
2020-12-09 13:18:56 +0800

    Can set 1 bit flags.

--- Builder.js
+++ Builder.js
@@ -6,43 +6,80 @@ class Builder {
         
         // default value
         this.id = 0;
-
+        this.flags = 0;
+        
         return this;
     }
-
+    
     addOutOfRange(name) {
         this.errors.push(`${name} is out of range`);
     }
-
-    invalidUInt16(val) {
+    
+    invalidUInt16Bit(val) {
         return val < 0 || val > 0xffff;
     }
-
-    // invalidOneBit(val) {
-    //     return val < 0 || val > 1;
-    // }
-
-    // setBit(i, u8, setOne) {
-    //     let oneMasks = 1 << i;
-    //     return setOne? u8 | oneMasks: u8 & ~oneMasks;
-    // }
-
+    
+    invalid1Bit(val) {
+        return val < 0 || val > 1;
+    }
+    
+    setBit(i, uint, setOne) {
+        let oneMasks = 1 << i;
+        return setOne? uint | oneMasks : uint & ~oneMasks;
+    }
+    
+    set_flag(i, bit, name) {
+        if (this.invalid1Bit(bit)) {
+            this.addOutOfRange(name);
+        } else {
+            this.flags = this.setBit(i, this.flags, bit === 1)
+        }
+    }
+    
     set_id(id) {
-        if (this.invalidUInt16(id))
+        if (this.invalidUInt16Bit(id)) {
             this.addOutOfRange('id');
-        else
+        }
+        else {
             this.id = id;
+        }
+
+        return this;
+    }
+    
+    set_qr(qr) {
+        this.set_flag(15, qr, 'qr');
+        return this;
     }
 
-    // set_qr(qr) {
-    //     if (!this.flags) this.flags = 0;
+    set_aa(aa) {
+        this.set_flag(10, aa, 'aa');
+        return this;
+    }
 
-    //     if (this.invalidOneBit(qr)) {
-    //         this.addOutOfRange('qr');
-    //     } else {
-    //         this.flags = this.setBit(15, this.flags, qr === 1)
-    //     }
-    // }
+    set_tc(tc) {
+        this.set_flag(9, tc, 'tc');
+        return this;
+    }
+
+    set_rd(rd) {
+        this.set_flag(8, rd, 'rd');
+        return this;
+    }
+
+    set_ra(ra) {
+        this.set_flag(7, ra, 'ra');
+        return this;
+    }
+
+    set_ad(ad) {
+        this.set_flag(5, ad, 'ad');
+        return this;
+    }
+
+    set_cd(cd) {
+        this.set_flag(4, cd, 'cd');
+    }
 }
 
 export default Builder;
--- spec/Builder.spec.mjs
+++ spec/Builder.spec.mjs
@@ -11,14 +11,23 @@ describe("Builder", () => {
         let over = 65536;
         let under = -1;
         let inside = 135;
-        expect(b.invalidUInt16(over)).toBe(true);
-        expect(b.invalidUInt16(under)).toBe(true);
-        expect(b.invalidUInt16(inside)).toBe(false);
+        expect(b.invalidUInt16Bit(over)).toBe(true);
+        expect(b.invalidUInt16Bit(under)).toBe(true);
+        expect(b.invalidUInt16Bit(inside)).toBe(false);
     })
 
     it("should set id", () => {
         let id = 135;
         b.set_id(id);
         expect(b.id).toBe(135);
+    })
+
+    it("should set flag", () => {
+        b.set_flag(15, 1, 'qr');
+        expect(b.flags).toBe(0b1000_0000_0000_0000);
+
+        b.flags = 0x800f;
+        b.set_flag(15, 0, 'qr');
+        expect(b.flags).toBe(0x000f);
     })
 })
